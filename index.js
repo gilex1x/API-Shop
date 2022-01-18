bf9c0739e435221d056b75e22d9ddc33875e97aa
Gilberto <gilbertosa099@outlook.com>
2022-01-17 19:36:10 -0500

    [ADD]Options to cors config

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const cors = require('cors');
 const routerApi = require('./src/routes/index');
 
 const {logError,errorHandler}= require('./src/middlewares/errorHandlers');
-
+const whitelist=[`http://localhost:${port}`];//Aca declaramos quien tiene acceso al API
+//Configuramos para que solo ciertos dominios tengan acceso
+const options={
+    origin:(origin,callback)=>{
+        if(whitelist.includes(origin)){
+            callback(null,true);
+        }else{
+            callback(new Error('No permite'));
+        }
+    }
+}
 const port = 3030;
 //agregamos el middleware para recibir datos en json
 app.use(express.json());
-app.use(cors()); 
+app.use(cors(options)); 
 
 app.get('/',(req,res)=>{
     res.send('Servidor andando');    
